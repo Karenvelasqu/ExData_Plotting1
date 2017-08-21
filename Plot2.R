6386bc83d5444430d012169b79ada73ec360748f
Karenvelasqu <camizapata90@gmail.com>
2017-08-21 16:01:11 -0500

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -10,6 +10,9 @@ datetime <- paste(as.Date(powerdata$Date), powerdata$Time)
 # to assign the new datatime to powerdata
 powerdata$Data_Time<-as.POSIXct(datetime)
 
+#Change Data to numeric
+powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)
+
 #Select time
 data1 <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")
 
@@ -19,5 +22,5 @@ head(data1)
 #Create plot
 
 png("plot2.png", width=480, height=480)
-plot(data1$Global_active_power~data1$Data_Time,type="l",ylab="Global active power (kilowatts",xlab="")
-dev.off
+with(data1, plot(data1$Data_Time, data1$Global_active_power, type="l", xlab="Day", ylab="Global Active Power (kilowatts)"))
+dev.off()
