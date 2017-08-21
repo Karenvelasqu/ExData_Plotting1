77929ee5165f4e7a57d70e070e5a6d53a1e1726c
Karenvelasqu <camizapata90@gmail.com>
2017-08-21 15:39:59 -0500

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -5,14 +5,7 @@ powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
 #Change Time Format
 powerdata$Time <- format(powerdata$Time, format="%H:%M:%S")
 #Change Data to numeric
-powerdata$Sub_metering_3 <- as.numeric(powerdata$Sub_metering_3)
 powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)
-powerdata$Global_reactive_power <- as.numeric(powerdata$Global_reactive_power)
-powerdata$Voltage <- as.numeric(powerdata$Voltage)
-powerdata$Global_intensity <- as.numeric(powerdata$Global_intensity)
-powerdata$Sub_metering_1 <- as.numeric(powerdata$Sub_metering_1)
-powerdata$Sub_metering_2 <- as.numeric(powerdata$Sub_metering_2)
-powerdata$Sub_metering_3 <- as.numeric(powerdata$Sub_metering_3)
 #Select Date
 data <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")
 #Create png
